c7657237d8358059b33b0605ee60a60fb710ce1c
mongoose11235813 <mongoose11235813@gamil.com>
2015-09-06 17:22:03 +0300

    Playing around with GUI

---
+++ Assets/UndraggableScrollRect.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+/// <summary>
+/// A scroll rect with mouse dragging disabled.
+/// </summary>
+public class UndraggableScrollRect : UnityEngine.UI.ScrollRect
+{
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+    }
+    public override void OnDrag(PointerEventData eventData)
+    {
+    }
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+    }
+}
